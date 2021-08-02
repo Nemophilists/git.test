3da804ec07fdf8f18703ab73524573f5eddbba21
Alex <3308296704@qq.com>
2021-08-02 21:15:56 +0800

    push test

--- src/main/java/test/vsc/c/Git_first.java
+++ src/main/java/test/vsc/c/Git_first.java
@@ -5,6 +5,7 @@ import org.omg.CORBA.UShortSeqHelper;
 public class Git_first {
     public static void main(String[] args) {
         System.out.println("测试idea分享至GitHub");
+        System.out.println("push test");
     }
 
 }
