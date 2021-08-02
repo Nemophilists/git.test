89325cf650351a2fb6daf92423c37d52b7b0b780
Alex <3308296704@qq.com>
2021-08-02 20:29:48 +0800

    third commit

--- src/main/java/test/vsc/c/Git_first.java
+++ src/main/java/test/vsc/c/Git_first.java
@@ -4,8 +4,7 @@ import org.omg.CORBA.UShortSeqHelper;
 
 public class Git_first {
     public static void main(String[] args) {
-        System.out.println("hello word");
-        System.out.println("77777");
+        System.out.println("冲突1");
     }
 
 }
