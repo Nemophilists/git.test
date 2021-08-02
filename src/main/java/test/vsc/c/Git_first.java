34292abbb2a4282797639c92e69a735b6ad43476
Alex <3308296704@qq.com>
2021-08-02 20:06:27 +0800

    firset commit

--- src/main/java/test/vsc/c/Git_first.java
+++ src/main/java/test/vsc/c/Git_first.java
@@ -1,8 +1,12 @@
 package test.vsc.c;
 
+import org.omg.CORBA.UShortSeqHelper;
+
 public class Git_first {
     public static void main(String[] args) {
         System.out.println("hello word");
+        UShortSeqHelper b;
+        System.out.println("hd666");
     }
 
 }
