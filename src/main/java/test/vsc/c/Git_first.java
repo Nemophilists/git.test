1cc623589db2d193ffe5ce611cf569721f5ef2d0
Alex <3308296704@qq.com>
2021-08-02 21:09:11 +0800

    idea test commit

--- src/main/java/test/vsc/c/Git_first.java
+++ src/main/java/test/vsc/c/Git_first.java
@@ -4,7 +4,7 @@ import org.omg.CORBA.UShortSeqHelper;
 
 public class Git_first {
     public static void main(String[] args) {
-        System.out.println("冲突1");
+        System.out.println("测试idea分享至GitHub");
     }
 
 }
